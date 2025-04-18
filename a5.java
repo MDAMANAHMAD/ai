5e7f57024baa77dcbb97f33fc93d71b5df90b7be
Aman <145838413+MDAMANAHMAD@users.noreply.github.com>
2025-04-18 23:03:01 +0530

    Create a5.java

---
+++ a5.java
@@ -0,0 +1,17 @@
+public class MaxDistanceCalculator {
+    public static int maxDistance(int c, int e, int w) {
+        int max = 0;
+        for (int f = 1; f <= c; f++) {
+            int r = c - f;
+            int d = f * e;
+            if (r >= w) d += (r - w) * e;
+            max = Math.max(max, d);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int dist = maxDistance(10, 20, 2);
+        System.out.println("Maximum distance: " + dist + " miles");
+    }
+}
